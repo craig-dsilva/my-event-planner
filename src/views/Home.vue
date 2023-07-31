3591b2050c387356f16719f3cb0564cdd7739c65
craig-dsilva <craig.s.dsilva@gmail.com>
2023-07-31 11:11:08 +0100

    Auth UI

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,10 +4,29 @@
   <div class="home">
     <h1>Home</h1>
     <p>This is the home page. It doesn't do much yet!</p>
+    <p v-if="isAdmin">You are signed in as admin.</p>
   </div>
 </template>
 
-
-<script lang="ts" setup>
-  
+<script lang="ts">
+import { Auth } from "aws-amplify";
+export default {
+  data() {
+    return {
+      isAdmin: false,
+    };
+  },
+  async mounted() {
+    try {
+      const userInfo = await Auth.currentAuthenticatedUser();
+      const groups =
+        userInfo.signInUserSession.accessToken.payload["cognito:groups"];
+      if (groups && groups.includes("admins")) {
+        this.isAdmin = true;
+      }
+    } catch (err) {
+      console.log("Error", err);
+    }
+  },
+};
 </script>
